2d6186be06904dff2fe0b6a872fd375471035f47
Nick <nsagias@gmail.com>
2021-12-22 12:46:23 -0500

    Create array of backgroundlist

--- expanding-cards/src/App.tsx
+++ expanding-cards/src/App.tsx
@@ -10,6 +10,15 @@ function App() {
   const FIVE_BACKGROUND_IMAGE = "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1203&q=80";
   // const DEFAULT_BACKGROUND_IMAGE = "";
 
+  const backgroundsList = [
+    {id: 1, ONE_BACKGROUND_IMAGE},
+    {id: 2, TWO_BACKGROUND_IMAGE},
+    {id: 3, THREE_BACKGROUND_IMAGE},
+    {id: 4, FOUR_BACKGROUND_IMAGE},
+    {id: 5, FIVE_BACKGROUND_IMAGE},
+  ];
+
+
   return (
     <div className="container">
       <div 
@@ -19,25 +28,25 @@ function App() {
       </div>
 
       <div 
-        className="panel" 
+        className={`panel  ${''}`}
         style={{backgroundImage: `url(${TWO_BACKGROUND_IMAGE})`}}>
         <h3>Portfolio Item Two</h3>
       </div>
 
       <div 
-        className="panel" 
+        className={`panel  ${''}`}
         style={{backgroundImage: `url(${THREE_BACKGROUND_IMAGE})`}}>
         <h3>Portfolio Item Three</h3>
       </div>
 
       <div 
-        className="panel" 
+        className={`panel  ${''}`}
         style={{backgroundImage: `url(${FOUR_BACKGROUND_IMAGE})`}}>
         <h3>Portfolio Item Four</h3>
       </div>
 
       <div 
-        className="panel" 
+        className={`panel  ${''}`}
         style={{backgroundImage: `url(${FIVE_BACKGROUND_IMAGE})`}}>
         <h3>Portfolio Item Five</h3>
       </div>
