f1c13d3fdee18c546ee1d80cb73937d0a301c3d4
Nick <nsagias@gmail.com>
2021-12-22 19:33:51 -0500

    Refactor data into backgroundListData, import backgroundimages there

--- expanding-cards/src/components/PanelList.tsx
+++ expanding-cards/src/components/PanelList.tsx
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
-
 import {
-  ONE_BACKGROUND_IMAGE,
-  TWO_BACKGROUND_IMAGE,
-  THREE_BACKGROUND_IMAGE,
-  FOUR_BACKGROUND_IMAGE,
-  FIVE_BACKGROUND_IMAGE,
   SET_DEFAULT_PANEL_ACTIVE
- } from './images/backgroundImages.jsx'
+ } from './images/backgroundImages.jsx';
+ import { backgroundsList }  from './helpers/backgroundLIstData';
 
 export default function PaneList() {
 
-  const backgroundsList = [
-    {id: 1, backgroundImg: ONE_BACKGROUND_IMAGE,   panelName: "Portfolio Item One",   panelActive: "one"}, 
-    {id: 2, backgroundImg: TWO_BACKGROUND_IMAGE,   panelName: "Portfolio Item Two",   panelActive: "two"},
-    {id: 3, backgroundImg: THREE_BACKGROUND_IMAGE, panelName: "Portfolio Item Three", panelActive: "three"},
-    {id: 4, backgroundImg: FOUR_BACKGROUND_IMAGE,  panelName: "Portfolio Item Four",  panelActive: "four"},
-    {id: 5, backgroundImg: FIVE_BACKGROUND_IMAGE,  panelName: "Portfolio Item Five",  panelActive: "five"},
-  ];
+  // const backgroundsList = [
+  //   {id: 1, backgroundImg: ONE_BACKGROUND_IMAGE,   panelName: "Portfolio Item One",   panelActive: "one"}, 
+  //   {id: 2, backgroundImg: TWO_BACKGROUND_IMAGE,   panelName: "Portfolio Item Two",   panelActive: "two"},
+  //   {id: 3, backgroundImg: THREE_BACKGROUND_IMAGE, panelName: "Portfolio Item Three", panelActive: "three"},
+  //   {id: 4, backgroundImg: FOUR_BACKGROUND_IMAGE,  panelName: "Portfolio Item Four",  panelActive: "four"},
+  //   {id: 5, backgroundImg: FIVE_BACKGROUND_IMAGE,  panelName: "Portfolio Item Five",  panelActive: "five"},
+  // ];
   
   const [panelActive, setPanelActive] = useState(SET_DEFAULT_PANEL_ACTIVE);
 
