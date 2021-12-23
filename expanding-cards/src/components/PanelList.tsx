2eb48a323c3ff3d8734f67f3c5ced5b5e8844113
Nick <nsagias@gmail.com>
2021-12-22 19:34:50 -0500

    Delete commented out code

--- expanding-cards/src/components/PanelList.tsx
+++ expanding-cards/src/components/PanelList.tsx
@@ -5,15 +5,6 @@ import {
  import { backgroundsList }  from './helpers/backgroundLIstData';
 
 export default function PaneList() {
-
-  // const backgroundsList = [
-  //   {id: 1, backgroundImg: ONE_BACKGROUND_IMAGE,   panelName: "Portfolio Item One",   panelActive: "one"}, 
-  //   {id: 2, backgroundImg: TWO_BACKGROUND_IMAGE,   panelName: "Portfolio Item Two",   panelActive: "two"},
-  //   {id: 3, backgroundImg: THREE_BACKGROUND_IMAGE, panelName: "Portfolio Item Three", panelActive: "three"},
-  //   {id: 4, backgroundImg: FOUR_BACKGROUND_IMAGE,  panelName: "Portfolio Item Four",  panelActive: "four"},
-  //   {id: 5, backgroundImg: FIVE_BACKGROUND_IMAGE,  panelName: "Portfolio Item Five",  panelActive: "five"},
-  // ];
-  
   const [panelActive, setPanelActive] = useState(SET_DEFAULT_PANEL_ACTIVE);
 
   const parsedBackgrounds = Array.isArray(backgroundsList) && backgroundsList.map(background => 
@@ -26,7 +17,6 @@ export default function PaneList() {
       <h3 >{background.panelName}</h3>
     </div>
   );
-
 
   return (
     <div className="container">
