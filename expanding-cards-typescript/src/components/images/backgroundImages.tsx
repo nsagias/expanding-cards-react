62b5db2cd14d64fddc0ddba03903b9e1dbcc698f
Nick <nsagias@gmail.com>
2021-12-22 23:14:47 -0500

    rename backgrounimages tsx

--- expanding-cards-typescript/src/components/images/backgroundImages.tsx
+++ expanding-cards-typescript/src/components/images/backgroundImages.tsx
@@ -1,3 +1,4 @@
+
 export const ONE_BACKGROUND_IMAGE = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
 export const TWO_BACKGROUND_IMAGE = "https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
 export const THREE_BACKGROUND_IMAGE = "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
