6430698904b0c4f9d34a4a876ff46298ff585a4c
Nick <nsagias@gmail.com>
2021-12-22 22:12:54 -0500

    Duplicate react version and make typescript version

---
+++ expanding-cards-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+import PaneList from './components/PanelList';
+
+
+function App() {
+  return (
+    <div>
+      <PaneList />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ expanding-cards-typescript/src/components/PanelList.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import {
+  SET_DEFAULT_PANEL_ACTIVE
+ } from './images/backgroundImages.jsx';
+ import { backgroundsList }  from './helpers/backgroundLIstData';
+
+export default function PaneList() {
+  const [panelActive, setPanelActive] = useState(SET_DEFAULT_PANEL_ACTIVE);
+
+  const parsedBackgrounds = Array.isArray(backgroundsList) && backgroundsList.map(background => 
+    <div 
+      className={`panel ${panelActive === background.panelActive ? 'active' : ''}`}
+      onClick={() => setPanelActive(background.panelActive)} 
+      key={background.id}
+      style={{backgroundImage: `url(${background.backgroundImg})`}}
+    >
+      <h3 >{background.panelName}</h3>
+    </div>
+  );
+
+  return (
+    <div className="container">
+      {parsedBackgrounds}
+    </div>
+  );
+}
